4e2b9c9eb30c7ca91e5c9921c6cf8f42ac46e0dd
La Min Ko <lminko.lmk@gmail.com>
2020-04-28 21:37:58 +0630

    add generic error handler

--- mAPN_service/app.py
+++ mAPN_service/app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from mAPN_service.routes import register_routes
 
 
 app = Flask(__name__)
 register_routes(app)
+
+
+@app.errorhandler(Exception)
+def generic_error_handler(error):
+    resp = make_response(jsonify(
+        message=error))
+    resp.headers['Content-Type'] = 'application/json'
+    return resp
